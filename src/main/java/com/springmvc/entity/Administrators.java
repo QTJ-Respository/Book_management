d423b732b8145f209d35e9b83b1001c947ecb597
qjt <qjt@qq.com>
2017-11-08 19:46:59 +0800

    two commit

---
+++ src/main/java/com/springmvc/entity/Administrators.java
@@ -0,0 +1,109 @@
+package com.springmvc.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Administrator on 2017/11/8 0008.
+ */
+@Entity
+@Table(name = "book_Admins")
+public class Administrators {
+    private int admins_no;//编号
+    private String admins_name;//姓名
+    private int admins_sex;//性别：1:男,0:女
+    private int admins_age;//年龄
+    private String admins_email;//邮箱
+    private String admins_tel;//电话
+    private String admins_address;//住址
+    private String admins_ID;//身份证
+    private int admins_state;//状态
+
+    public Administrators() {
+    }
+
+    public Administrators(int admins_no, String admins_name, int admins_sex, int admins_age, String admins_email, String admins_tel, String admins_address, String admins_ID, int admins_state) {
+        this.admins_no = admins_no;
+        this.admins_name = admins_name;
+        this.admins_sex = admins_sex;
+        this.admins_age = admins_age;
+        this.admins_email = admins_email;
+        this.admins_tel = admins_tel;
+        this.admins_address = admins_address;
+        this.admins_ID = admins_ID;
+        this.admins_state = admins_state;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getAdmins_no() {
+        return admins_no;
+    }
+
+    public void setAdmins_no(int admins_no) {
+        this.admins_no = admins_no;
+    }
+
+    public String getAdmins_name() {
+        return admins_name;
+    }
+
+    public void setAdmins_name(String admins_name) {
+        this.admins_name = admins_name;
+    }
+
+    public int getAdmins_sex() {
+        return admins_sex;
+    }
+
+    public void setAdmins_sex(int admins_sex) {
+        this.admins_sex = admins_sex;
+    }
+
+    public int getAdmins_age() {
+        return admins_age;
+    }
+
+    public void setAdmins_age(int admins_age) {
+        this.admins_age = admins_age;
+    }
+
+    public String getAdmins_email() {
+        return admins_email;
+    }
+
+    public void setAdmins_email(String admins_email) {
+        this.admins_email = admins_email;
+    }
+
+    public String getAdmins_tel() {
+        return admins_tel;
+    }
+
+    public void setAdmins_tel(String admins_tel) {
+        this.admins_tel = admins_tel;
+    }
+
+    public String getAdmins_address() {
+        return admins_address;
+    }
+
+    public void setAdmins_address(String admins_address) {
+        this.admins_address = admins_address;
+    }
+
+    public String getAdmins_ID() {
+        return admins_ID;
+    }
+
+    public void setAdmins_ID(String admins_ID) {
+        this.admins_ID = admins_ID;
+    }
+
+    public int getAdmins_state() {
+        return admins_state;
+    }
+
+    public void setAdmins_state(int admins_state) {
+        this.admins_state = admins_state;
+    }
+}
--- src/main/webapp/pages/Main.jsp
+++ src/main/webapp/pages/Main.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: Administrator
   Date: 2017/11/8 0008
-  Time: 下午 7:07
+  Time: 下午 7:31
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,11 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-<h1>用户登录</h1>
-<form action="" method="post">
-    用户名：<input name="uname"><br>
-    密&nbsp;码：<input name="upwd"><br>
-    <input type="submit" value="登录">
-</form>
+
 </body>
 </html>
